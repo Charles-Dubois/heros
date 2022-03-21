df5f12b52a49d10fd399f89ebad920ffdee9679c
Charles Dubois <charles.dubois.h@gmail.com>
2022-03-21 17:01:59 +0100

    database added

--- back/index.js
+++ back/index.js
@@ -1,62 +1,52 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { Pool } = require("pg");
 const app = express();
 app.use(express.json());
-const superHeros = [
-  {
-    name: "Iron Man",
-    power: ["money"],
-    color: "red",
-    isAlive: true,
-    age: 46,
-    image:
-      "https://blog.fr.playstation.com/tachyon/sites/10/2019/07/unnamed-file-18.jpg?resize=1088,500&crop_strategy=smart",
-  },
-  {
-    name: "Thor",
-    power: ["electricity", "worthy"],
-    color: "blue",
-    isAlive: true,
-    age: 300,
-    image:
-      "https://www.bdfugue.com/media/catalog/product/cache/1/image/400x/17f82f742ffe127f42dca9de82fb58b1/9/7/9782809465761_1_75.jpg",
-  },
-  {
-    name: "Daredevil",
-    power: ["blind"],
-    color: "red",
-    isAlive: false,
-    age: 30,
-    image:
-      "https://aws.vdkimg.com/film/2/5/1/1/251170_backdrop_scale_1280xauto.jpg",
-  },
-];
-app.use((_req, _res, next) => {
-  console.log("ceci est un middleware");
-  next();
+dotenv.config({
+  path: "./config.env",
 });
+const Postgres = new Pool({ ssl: { rejectUnauthorized: false } });
+
+// app.use((_req, _res, next) => {
+//   console.log("ceci est un middleware");
+//   next();
+// });
 app.get("/", (_req, res) => {
   res.send(
     "endpoint (/heroes) to show all heroes \n endpoint (/heroes/HeroesName) to show a hero"
   );
 });
 
-app.get("/heroes", (req, res) => {
-  if (!req.params.heroes) {
-    return res.json(superHeros);
-  } else {
-    superHeros.find((hero) => {
-      return hero.name === req.params.name;
-    });
+app.get("/heroes", async (req, res) => {
+  let heroes;
+
+  try {
+    heroes = await Postgres.query("SELECT * FROM heroes");
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Error" });
   }
+  const result = heroes.rows;
+  res.json(result);
 });
 
-app.get("c", (req, res) => {
-  const heroName = () =>
-    superHeros.find((hero) => {
-      return hero.name === req.params.name;
-    });
-  res.send(heroName());
+app.get("/heroes/:name", async (req, res) => {
+  let heroes;
+  try {
+    heroes = await Postgres.query(
+      "SELECT * FROM heroes WHERE LOWER(name)= $1",
+      [req.params.name.toLowerCase()]
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Error" });
+  }
+
+  const result = heroes.rows;
+  res.json(result);
 });
+
 app.delete("/heroes/:name", (req, res) => {
   const heroName = () =>
     superHeros.find((hero) => {
@@ -66,41 +56,63 @@ app.delete("/heroes/:name", (req, res) => {
   res.send(heroName);
 });
 
-app.get("/heroes/:name/powers", (req, res) => {
-  const heroName = () =>
-    superHeros.find((hero) => {
-      return hero.name === req.params.name;
-    });
-  res.send(heroName().power);
-});
-
-app.post("/heroes", (req, res, next) => {
-  req.body.name = req.body.name.toLowerCase();
-
-  const checkHero = () =>
-    superHeros.find((hero) => {
-      return req.body.name === hero.name;
-    });
-
-  console.log(req.body.name);
-  if (!checkHero()) {
-    next();
-  } else {
-    res.send("error this super hero already exists");
+app.get("/heroes/:name/powers", async (req, res) => {
+  let heroes;
+  try {
+    heroes = await Postgres.query(
+      "SELECT power FROM heroes WHERE LOWER(name)= $1",
+      [req.params.name.toLowerCase()]
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Error" });
   }
+
+  const result = heroes.rows;
+  res.json(result);
 });
-app.post("/heroes", (req, res) => {
-  superHeros.push(req.body);
-  res.send("Ok, héros ajouté");
+
+app.post("/heroes", async (req, res, next) => {
+  add = req.body;
+  try {
+    await Postgres.query(
+      "INSERT INTO heroes(name, power, color, isAlive, age, image) VALUES($1, $2, $3 , $4 , $5, $6)",
+      [add.name, add.power, add.color, add.isAlive, add.age, add.image]
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Error" });
+  }
+  res.send("hero added");
 });
-app.patch("/heroes/:name/powers", (req, res) => {
-  const heroName = () =>
-    superHeros.find((hero) => {
-      return hero.name === req.params.name;
-    });
-  heroName().power.push(req.body.power);
-  res.send("Pouvoir ajouté !");
+
+app.patch("/heroes/:name/powers", async (req, res) => {
+  let heroes;
+  try {
+    heroes = await Postgres.query(
+      "SELECT power FROM heroes WHERE LOWER(name)= $1",
+      [req.params.name.toLowerCase()]
+    );
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Error" });
+  }
+  const result = heroes.rows[0].power;
+
+  result.push(req.body.power);
+
+  try {
+    await Postgres.query("UPDATE heroes SET power = $1 WHERE LOWER(name)= $2", [
+      result,
+      req.params.name.toLowerCase(),
+    ]);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ message: "Error" });
+  }
+  res.send("power added");
 });
+
 app.get("*", (_req, res) => {
   res.send("error 404");
 });
